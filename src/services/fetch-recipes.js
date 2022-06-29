7be1442013b335765cb345da4f5506de0d7a4a32
Will Gundy <willgunderson503@gmail.com>
2022-06-29 12:42:23 -0700

    fixed fetch using env variables

--- src/services/fetch-recipes.js
+++ src/services/fetch-recipes.js
@@ -1,5 +1,5 @@
 export async function getAllRecipes() {
-  const resp = await fetch(`http://localhost:7890/api/v1/recipes`);
+  const resp = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/recipes`);
   console.log(JSON.stringify(resp.body));
   console.log('resp', resp);
   return resp.json();
