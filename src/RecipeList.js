4c864fb9ab7ac9799742083a19581f1501aea456
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-06-29 14:45:53 -0700

    wrote form for recipes page

--- src/RecipeList.js
+++ src/RecipeList.js
@@ -1,12 +1,47 @@
 import React from 'react';
 import Recipe from './Recipe';
+import { useState } from 'react';
 
 export default function RecipeList({ recipes }) {
+  const [recipeInForm, setRecipeInForm] = useState({
+    title: '',
+    description: '',
+    prepTime: 0,
+    cookTime: 0
+  });
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    // await createRecipe(recipeInForm);
+  }
+
   return (
     <div>
-      {recipes.map((recipe, i) => 
-        <Recipe recipe={recipe} key={i} />
-      )}
+      <div>
+        {recipes.map((recipe, i) => 
+          <Recipe recipe={recipe} key={i} />
+        )}
+      </div>
+      <form onSubmit={handleSubmit}>
+        <h2>Create New Recipe</h2>
+        <label>Title
+          <input defaultValue="" onChange={(e) => setRecipeInForm({ ...recipeInForm, title: e.target.value })} name="title">
+          </input>
+        </label>
+        <label>Description
+          <input onChange={(e) => setRecipeInForm({ ...recipeInForm, description: e.target.value })} required name="description">
+          </input>
+        </label>
+        <label>Prep Time
+          <input onChange={(e) => setRecipeInForm({ ...recipeInForm, prepTime: e.target.value })} required name="prepTime" type="number">
+          </input>
+        </label>
+        <label>Cook Time
+          <input onChange={(e) => setRecipeInForm({ ...recipeInForm, cookTime: e.target.value })} required name="cookTime" type="number">
+          </input>
+        </label>
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
