10ee19829d71a87695e432af003c02b01c7399f2
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-06-29 15:32:29 -0700

    wrote getRecipeById function for fetch-recipes file

--- src/services/fetch-recipes.js
+++ src/services/fetch-recipes.js
@@ -3,6 +3,18 @@ export async function getAllRecipes() {
   return resp.json();
 }
 
+export async function getRecipeById(id) {
+  const resp = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/recipes/${id}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    credentials: 'include',
+    mode: 'cors',
+  });
+  return resp.json();
+}
+
 export async function createRecipe(newRecipe) {
   const resp = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/recipes`, {
     method: 'POST',
