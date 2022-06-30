4dba55db5821393627a9e4a2417592307b9ceb46
Cody-Wise <cody@sharptackmedia.com>
2022-06-30 14:28:46 -0700

    Added delete button and delete function to recipe js

--- src/Recipe.js
+++ src/Recipe.js
@@ -1,12 +1,16 @@
 import React from 'react';
 
 export default function Recipe({ recipe }) {
+  async function handleDelete(id) {
+    await deleteRecipe(id);
+  }
   return (
     <div>
       <h3>{recipe.title}</h3>
       <p>{recipe.description}</p>
       <p>{recipe.cookTime}</p>
       <p>{recipe.prepTime}</p>
+      <button onClick={handleDelete}>Delete</button>
     </div>
   );
 }
