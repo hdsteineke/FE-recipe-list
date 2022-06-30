79fa67f755f2ccb1c715af9866fb64ca81482ab6
Cody-Wise <cody@sharptackmedia.com>
2022-06-30 15:18:14 -0700

    Added form for updated recipe and handleUpdate function

--- src/RecipeDetail.js
+++ src/RecipeDetail.js
@@ -6,6 +6,7 @@ import { getRecipeById } from './services/fetch-recipes';
 export default function RecipeDetail() {
   const { id } = useParams();
   const [recipeDetails, setRecipeDetails] = useState({});
+  const [updateRecipeInForm, setUpdateRecipeInForm] = useState({});
 
   useEffect(() => {
     async function getRecipe() {
@@ -15,10 +16,63 @@ export default function RecipeDetail() {
     getRecipe();
   }, [id]);
 
+  async function handleUpdate() {
+    await updateRecipe({ id, ...updateRecipeInForm });
+  }
+
   return (
-    <div>RecipeDetail
-      <h3>{recipeDetails.title}</h3>
-      <p>{recipeDetails.description}</p>
+    <div>
+      <div>
+        RecipeDetail
+        <h3>{recipeDetails.title}</h3>
+        <p>{recipeDetails.description}</p>
+      </div>
+      <form onSubmit={handleUpdate}>
+        <h2>Update Recipe</h2>
+        <label>
+          Title
+          <input
+            defaultValue=""
+            onChange={(e) =>
+              setUpdateRecipeInForm({ ...updateRecipeInForm, title: e.target.value })
+            }
+            name="title"
+          ></input>
+        </label>
+        <label>
+          Description
+          <input
+            onChange={(e) =>
+              setUpdateRecipeInForm({ ...updateRecipeInForm, description: e.target.value })
+            }
+            required
+            name="description"
+          ></input>
+        </label>
+        <label>
+          Prep Time
+          <input
+            onChange={(e) =>
+              setUpdateRecipeInForm({ ...updateRecipeInForm, prepTime: e.target.value })
+            }
+            required
+            name="prepTime"
+            type="number"
+          ></input>
+        </label>
+        <label>
+          Cook Time
+          <input
+            onChange={(e) =>
+              setUpdateRecipeInForm({ ...updateRecipeInForm, cookTime: e.target.value })
+            }
+            required
+            name="cookTime"
+            type="number"
+          ></input>
+        </label>
+        <button>Update</button>
+      </form>
     </div>
   );
 }
