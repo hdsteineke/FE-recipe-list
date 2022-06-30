ac8f70c0aa91a5eeab996ac04533e2dd399b2bb4
Cody-Wise <cody@sharptackmedia.com>
2022-06-30 14:37:48 -0700

    Added delete recipe function to fetch-recipes.js

--- src/services/fetch-recipes.js
+++ src/services/fetch-recipes.js
@@ -7,7 +7,7 @@ export async function getRecipeById(id) {
   const resp = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/recipes/${id}`, {
     method: 'GET',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
     credentials: 'include',
     mode: 'cors',
@@ -19,11 +19,23 @@ export async function createRecipe(newRecipe) {
   const resp = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/recipes`, {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
     credentials: 'include',
     mode: 'cors',
-    body: JSON.stringify(newRecipe)
+    body: JSON.stringify(newRecipe),
+  });
+  return resp.json();
+}
+
+export async function deleteRecipe(id) {
+  const resp = await fetch(`${process.env.REACT_APP_API_URL}/api/v1/recipes/${id}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+    mode: 'cors',
   });
   return resp.json();
 }
